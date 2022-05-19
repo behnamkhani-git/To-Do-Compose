9a59e54f1d048ac7cd1e7550fa74547921af128d
Behnam Khani <behnamkhani@hotmail.com>
2022-05-19 12:14:10 +0430

    Setup Dagger-Hilt

--- app/src/main/java/khani/behnam/to_docompose/data/ToDoDatabase.kt
+++ app/src/main/java/khani/behnam/to_docompose/data/ToDoDatabase.kt
@@ -1,9 +1,10 @@
 package khani.behnam.to_docompose.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import khani.behnam.to_docompose.data.models.ToDoTask
 
 @Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
-abstract class ToDoDatabase {
+abstract class ToDoDatabase: RoomDatabase() {
     abstract fun toDoDao(): ToDoDao
 }
---
+++ app/src/main/java/khani/behnam/to_docompose/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package khani.behnam.to_docompose.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import khani.behnam.to_docompose.data.ToDoDatabase
+import khani.behnam.to_docompose.util.Constants
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        ToDoDatabase::class.java,
+        Constants.DATABASE_NAME
+    )
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+}
